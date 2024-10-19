128c8d8ab63b698f30ed3c8e65fb4e66cff4606e
Krasilnikov Roman <x0kill@ya.ru>
2024-10-19 21:42:27 +0300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    mk.url = "github:x0k/mk";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      mk,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            mk.packages.${system}.default
+            pkgs.go_1_23
+            pkgs.air
+            pkgs.golangci-lint
+          ];
+        };
+      };
+    };
+}
